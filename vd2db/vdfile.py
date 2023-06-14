c4a130fec38e6177f70ac27d9f397e876a6a8805
corralien <damien.corral@gmail.com>
2023-06-14 14:56:06 +0200

    fix bugs

--- vd2db/vdfile.py
+++ vd2db/vdfile.py
@@ -4,8 +4,9 @@ import pathlib
 DIMENSIONS = ['Attribute', 'Commodity', 'Process', 'Period', 'Region', 'Vintage', 'TimeSlice', 'UserConstraint', 'PV']
 
 
-def read_vdfile(vdfile):
+def read_vdfile(vdfile: pathlib.Path) -> pd.DataFrame:
     params = {'comment': '*', 'header': None, 'dtype': object, 'na_values': ['-', 'NONE']}
     veda = pd.read_csv(vdfile, names=DIMENSIONS, **params).astype({'PV': float})
     veda.insert(0, 'Scenario', vdfile.stem)
     return veda
+
