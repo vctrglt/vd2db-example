179f53324affc1ec709b96137086282b1509f159
corralien <damien.corral@gmail.com>
2023-10-23 14:56:13 +0200

    fix import and remove commands
    - import scenario only if it does not exist
    - add pragma on sqlite (foreign_key and auto_vacuum)
    - create right relationships between tables (delete cascade)
    - remove scenario is now consistent

--- vd2db/console.py
+++ vd2db/console.py
@@ -1,26 +1,42 @@
 import click
 import pandas as pd
 import pathlib
-from sqlalchemy.engine import create_engine, URL
+from sqlalchemy.engine import create_engine, URL, Engine
 from sqlalchemy.schema import MetaData, Table, Column, ForeignKey
 from sqlalchemy.sql import insert, select, text, delete
 from sqlalchemy.types import String, Integer, Float, DateTime
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.event import listens_for
+from sqlalchemy.ext.automap import generate_relationship, interfaces
 from datetime import datetime
-from .vdfile import read_vdfile
+from vd2db.vdfile import read_vdfile
 from rich.progress import Progress, TextColumn, BarColumn, TaskProgressColumn, MofNCompleteColumn
+from sqlite3 import Connection as SQLite3Connection
 
-
-DIMENSIONS = ['Scenario', 'Sow', 'Commodity', 'Process', 'Period',
-              'Region', 'Vintage', 'TimeSlice', 'UserConstraint']
+DIMENSIONS = ['Scenario', 'Attribute', 'Sow', 'Commodity', 'Process',
+              'Period', 'Region', 'Vintage', 'TimeSlice', 'UserConstraint']
 
 APP_NAME = 'vd2db'
-APP_VERSION = '0.1'
 
 CONFIG_DIR = pathlib.Path(click.get_app_dir(APP_NAME))
 DATA_DIR = pathlib.Path.home() / APP_NAME
 
-fmt = '{desc:<25.20}{percentage:3.0f}%|{bar:40}|{n_fmt:>3}/{total_fmt:>3}'
+
+# enable foreign keys. if you do not do this, ON DELETE CASCADE fails silently!
+@listens_for(Engine, 'connect')
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute('PRAGMA foreign_keys=ON;')
+        cursor.execute('PRAGMA auto_vacuum=FULL;')
+        cursor.close()
+
+
+def _generate_relationship(base, direction, return_fn, attrname, local_cls, referred_cls, **kw):
+    if direction is interfaces.ONETOMANY:
+        kw['cascade'] = 'all, delete'
+        kw['passive_deletes'] = True
+    return generate_relationship(base, direction, return_fn, attrname, local_cls, referred_cls, **kw)
 
 
 @click.group()
@@ -36,19 +52,22 @@ def init_database(dbname):
     """Initialize a new database."""
     db = DATA_DIR / f'{dbname}.db'
     engine = create_engine(URL.create('sqlite', database=str(db)), echo=False)
+    # engine = create_engine(URL.create('mysql', host='127.0.0.1', username='root', database=dbname), echo=False)
     metadata = MetaData()
 
     # Create dimension tables
-    tables = {'Scenario': Table('Scenario', metadata,
-                                Column('ID', Integer, primary_key=True),
-                                Column('Name', String(255)),
-                                Column('created_at', DateTime, default=datetime.now),
-                                Column('updated_at', DateTime, default=datetime.now, onupdate=datetime.now))}
+    Table('Scenario',
+          metadata,
+          Column('ID', Integer, primary_key=True),
+          Column('Name', String(255), unique=True),
+          Column('created_at', DateTime, default=datetime.now),
+          Column('updated_at', DateTime, default=datetime.now, onupdate=datetime.now))
 
-    tables |= {dim: Table(dim, metadata,
-                          Column('ID', Integer, primary_key=True),
-                          Column('Name', String(255)))
-               for dim in DIMENSIONS[1:]}
+    for dim in DIMENSIONS[1:]:
+        Table(dim,
+              metadata,
+              Column('ID', Integer, primary_key=True),
+              Column('Name', String(255)))
 
     metadata.create_all(engine)
 
@@ -59,15 +78,15 @@ def list_scenarios(dbname):
     """List scenarios in specified database."""
     db = DATA_DIR / f'{dbname}.db'
     engine = create_engine(URL.create('sqlite', database=str(db)), echo=False)
+    # engine = create_engine(URL.create('mysql', host='127.0.0.1', username='root', database=dbname), echo=False)
     Base = automap_base()
     Base.prepare(engine)
-    metadata = Base.metadata
 
     with engine.connect() as con:
         scenarios = con.execute(select(Base.classes['Scenario'].Name)).all()
-        print(f'{len(scenarios)} scenario(s) found in "{dbname}" database:')
+        click.echo(f'{len(scenarios)} scenario(s) found in "{dbname}" database:')
         for scen in scenarios:
-            print(f'- {scen.Name}')
+            click.echo(f'- {scen.Name}')
 
 
 @click.command(name='import')
@@ -77,41 +96,49 @@ def import_scenario(vdfile, dbname):
     """Import specified scenario."""
     db = DATA_DIR / f'{dbname}.db'
     engine = create_engine(URL.create('sqlite', database=str(db)), echo=False)
-    Base = automap_base()
+    # engine = create_engine(URL.create('mysql', host='127.0.0.1', username='root', database=dbname), echo=False)
+    Base = automap_base()  # noqa: N806
     Base.prepare(engine)
-    metadata = Base.metadata
 
     # Read VD File
-    veda = read_vdfile(vdfile)
+    scenario, veda = read_vdfile(vdfile)
+
+    # Check if the scenario already exists
+    with engine.connect() as con:
+        Scenario = Base.classes['Scenario']
+        row = con.execute(select(Scenario).where(Scenario.Name == scenario)).first()
+        if row:
+            click.echo(f'Scenario "{row.Name}" already exists.')
+            click.echo('Use "remove" command first to delete it then "import" again.')
+            return 1
 
     # Explode data into multiple dataframes
     dataset = {}
     for attr, df in veda.groupby('Attribute'):
-        dataset[attr] = df.loc[:, ~df.isna().all()].drop(columns='Attribute')
+        dataset[attr] = df.loc[:, ~df.isna().all()]
 
-    # Create missing tables and views
+    # Create missing attribute tables and their view
     with engine.connect() as con:
         for name, df in dataset.items():
-            if name not in Base.classes:
+            if f'_{name}' not in Base.classes:
+                # Create attribute table
+                Table(
+                    f'_{name}',
+                    Base.metadata,
+                    Column('ID', Integer, primary_key=True),
+                    *[Column(col, Integer, ForeignKey(f'{col}.ID', ondelete='CASCADE'), nullable=True)
+                      for col in df.columns[:-1]],
+                    Column('PV', Float)
+                ).create(con)
 
-                # Table factory
-                params = [Column('ID', Integer, primary_key=True)]
-                for col in df.columns[:-1]:
-                    params.append(Column(col, Integer, ForeignKey(f'{col}.ID', onupdate='CASCADE', ondelete='CASCADE')))
-                params.append(Column('PV', Float))
-                # tables.append(Table(name, metadata, *params))
-                Table(name, metadata, *params).create(con)
-
-                # View factory
-                part1 = [f'{col}.Name AS {col}' for col in df.columns[:-1]]
-                part1.insert(1, f"'{name}' AS Attribute")
-                part1.append('PV')
-                part2 = [f'LEFT JOIN {col} ON {name}.{col} = {col}.ID' for col in df.columns[:-1]]
-                stmt = f"""CREATE VIEW vd_{name} AS SELECT {', '.join(part1)} FROM {name} {' '.join(part2)}"""
+                # Create associated view
+                part1 = [f'{col}.Name AS {col}' for col in df.columns[:-1]] + ['PV']
+                part2 = [f'LEFT JOIN {col} ON _{name}.{col} = {col}.ID' for col in df.columns[:-1]]
+                stmt = f"""CREATE VIEW {name} AS SELECT {', '.join(part1)} FROM _{name} {' '.join(part2)}"""
                 con.execute(text(stmt))
         con.commit()
 
-    # Reload mapped classes
+    # Reload attribute tables
     Base.prepare(engine)
 
     # Load dictionaries
@@ -120,20 +147,20 @@ def import_scenario(vdfile, dbname):
                    for dim in veda.columns.intersection(DIMENSIONS)}
         indexes = {}
         for dim, data in uniques.items():
-            stmt = select(Base.classes[dim])
 
             # Load existing data
-            cur = con.execute(stmt)
+            cur = con.execute(select(Base.classes[dim]))
             df = pd.DataFrame.from_records(cur, columns=cur.keys())
-            new_data = data[~data['Name'].isin(df['Name'])]
 
-            # Insert new data and reload it
+            # Insert new data
+            new_data = data[~data['Name'].isin(df['Name'])]
             if not new_data.empty:
                 con.execute(insert(Base.classes[dim]), new_data.to_dict('records'))
                 con.commit()
-                cur = con.execute(stmt)
-                df = pd.DataFrame.from_records(cur, columns=cur.keys())
 
+            # Reload data to be sure
+            cur = con.execute(select(Base.classes[dim]))
+            df = pd.DataFrame.from_records(cur, columns=cur.keys())
             indexes[dim] = df.set_index('Name')['ID']
 
     with Progress(
@@ -144,17 +171,12 @@ def import_scenario(vdfile, dbname):
         transient=True
     ) as progress:
         with engine.connect() as con:
-            # for attr, df in tqdm(dataset.items(), bar_format=fmt, desc=vdfile.stem, unit='tables'):
             for attr, df in progress.track(dataset.items(), description=f'[yellow]Processing "{vdfile.name}"'):
-                df = df.replace(indexes)
-                con.execute(insert(Base.classes[attr]), df.to_dict('records'))
+                cols = df.columns.intersection(indexes)
+                df[cols] = df[cols].apply(lambda x: x.map(indexes[x.name].astype('Int32')))
+                con.execute(insert(Base.classes[f'_{attr}']), df.to_dict('records'))
                 con.commit()
             progress.print(f'[green]Processed "{vdfile.name}": {len(dataset)} tables - {len(veda)} records')
-
-
-@click.command(name='update')
-def update_scenario():
-    """Update an existing scenario."""
 
 
 @click.command(name='remove')
@@ -168,19 +190,17 @@ def remove_scenario(scenario, dbname):
     Base.prepare(engine)
 
     with engine.connect() as con:
-        scenario_table = Base.classes['Scenario']
-        con.execute(delete(scenario_table).where(scenario_table.Name == scenario))
+        Scenario = Base.classes['Scenario']
+        con.execute(delete(Scenario).where(Scenario.Name == scenario))
         con.commit()
 
 
 cli.add_command(init_database)
 cli.add_command(list_scenarios)
 cli.add_command(import_scenario)
-cli.add_command(update_scenario)
 cli.add_command(remove_scenario)
 cli.epilog = f"Run 'vd2db COMMAND --help' for more information on a command."
 
 
 if __name__ == '__main__':
     cli()
-
--- vd2db/vdfile.py
+++ vd2db/vdfile.py
@@ -10,9 +10,11 @@ def read_vdfile(vdfile: pathlib.Path) -> pd.DataFrame:
             if sre := re.search(r'^\*\s*(?P<key>[^-]+)-\s*(?P<val>.+)', row):
                 params[sre.group('key')] = sre.group('val')
 
+        scenario = params['ImportID'].split(':')[-1]
+
         options = {'comment': '*', 'header': None, 'dtype': object, 'na_values': ['-', 'NONE'],
                    'sep': params['FieldSeparator'], 'quotechar': params['TextDelim'], 'quoting': 2}
 
-        veda = pd.read_csv(fp, names=params['Dimensions'].split(';'), **options).astype({'PV': float})
-        veda.insert(0, 'Scenario', params['ImportID'].split(':')[-1])
-        return veda
+        veda = pd.read_csv(fp, names=params['Dimensions'].split(';'), **options).astype({params['ValueDim']: float})
+        veda.insert(0, 'Scenario', scenario)
+        return scenario, veda
